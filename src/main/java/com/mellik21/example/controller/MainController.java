a6317421d376de5bddd936ca7e76de51bb7fe6ae
mellik21 <korolevazaek@gmail.com>
2021-07-30 23:01:24 +0400

    Changed mustache to freemarker

--- src/main/java/com/mellik21/example/controller/MainController.java
+++ src/main/java/com/mellik21/example/controller/MainController.java
@@ -5,6 +5,7 @@ import com.mellik21.example.model.User;
 import com.mellik21.example.repos.MessageRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,9 +27,19 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(
+            @RequestParam(required = false,defaultValue = "")String filter,
+            Model model) {
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -45,15 +56,4 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/main/filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-        model.put("messages", messages);
-        return "main";
-    }
 }
---
+++ src/main/resources/templates/parts/common.ftl
@@ -0,0 +1,14 @@
+<#macro page>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Example</title>
+</head>
+
+<body>
+<#nested>
+</body>
+
+</html>
+</#macro>
